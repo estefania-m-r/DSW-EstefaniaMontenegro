01bc7561955f1e32ee55240bcf73ec17f0322fe5
estefania-m-r <estefania_m_r@hotmail.com>
2021-09-05 08:40:03 -0500

    Iniciando
    Mi primera pagina web con Visual Studio Code

---
+++ index.bat
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sitio Web 2021</title>
+</head>
+<body>
+    <h1>Proyecto de sitio Web 2021 - 16-4</h1>
+    <hr>
+    <h3> <em> Estefania I Montenegro </em>  <br> programa de Ingeniería de Sistemas, <br> creado el 5-09-2021</h3>
+    <p>este es un parrafo, pero vamos a crear <br> un salto de linea</p>
+
+    <p>Aquí creamos otro parrafo</p>
+</body>
+</html>
